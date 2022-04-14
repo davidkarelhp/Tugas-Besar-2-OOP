d6cf1eed6d9cfa52a0162772e89014b282715537
nandono206 <79740572+nandono206@users.noreply.github.com>
2022-04-14 16:45:08 +0700

    add Board

---
+++ src/main/java/com/aetherwars/Board.java
@@ -0,0 +1,34 @@
+package com.aetherwars;
+
+public class Board {
+
+    private Character[] Board;
+
+    // Konstruktor
+    public Board(){
+        this.Board = new Character[5];
+    }
+
+    public void putCardInSlot(int slot, Character card){
+        if (slot < 5 && slot > -1){
+            this.Board[slot] = card;
+        }
+    }
+
+    public void attackFromSlot(int slot, Board enemyBoard, int enemySlot){
+        if (slot <5 && slot > -1 && enemySlot < 5 && enemySlot > -1){
+            this.Board[slot].attackEnemy(enemyBoard.Board[enemySlot]);
+            this.Board[slot].receiveExperience();
+        }
+    }
+
+    public void attackFromSlot(int slot, Player enemy){
+        if (slot <5 && slot > -1){
+            this.Board[slot].attackPlayer(enemy);
+            this.Board[slot].receiveExperience();
+        }
+    }
+
+
+
+}
