70502b996fe364a1a9dc8b156db5799d6f3e29bb
Nadia Mareta <nadia.leiden6@gmail.com>
2022-04-19 22:40:37 +0700

    add player first

---
+++ src/main/java/com/aetherwars/model/Player.java
@@ -0,0 +1,9 @@
+package com.aetherwars.model;
+
+public class Player {
+    protected String nama;
+    protected double HealthPoints;
+    protected int Mana;
+    protected int deck;
+    protected String ImagePath;
+}
