f344a8d72c6da402983b14ec1f4414bad4be1985
Nadia Mareta <nadia.leiden6@gmail.com>
2022-04-19 22:54:29 +0700

    add player

--- src/main/java/com/aetherwars/model/Player.java
+++ src/main/java/com/aetherwars/model/Player.java
@@ -1,9 +1,28 @@
 package com.aetherwars.model;
 
 public class Player {
-    protected String nama;
-    protected double HealthPoints;
-    protected int Mana;
-    protected int deck;
-    protected String ImagePath;
+    private String playerName;
+    private double HealthPoints;
+    private int Mana;
+
+    //ini nanti diganti sama list deck
+    private int deck;
+    private String hand_deck;
+    private String board_deck;
+
+    public void getCard(int deck){
+        //ngambil dari deck
+    }
+
+    public void seeCard (String board_deck, String hand_deck){
+        //ngelihat dulu deskripsinya
+    }
+
+    public void moveCard (String hand_deck, String board_deck){
+        //pindahin dari list ini ke list yang satu lagi
+    }
+
+    public void turnPhase(){
+        //pindah ke fase(?) ini sama kayak game state apa gimana?
+    }
 }
