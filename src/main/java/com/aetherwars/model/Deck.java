474a2de33afa1f444bd767d216a968dede439613
roastland <tfpruslanali@gmail.com>
2022-04-25 06:41:04 +0700

    update deck

--- src/main/java/com/aetherwars/model/Deck.java
+++ src/main/java/com/aetherwars/model/Deck.java
@@ -13,7 +13,23 @@ public class Deck {
     public Deck() {
         this.deck = new Stack<>();
         // Harus dipikirin cara membentuk deck-nya pas awal-awal
-
+        
+        // ganti sama daftar card yg ada int[] initCard = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0};
+        ArrayList<Card> listCard = arrayToList(initCard);
+        ArrayList<Card> result = new ArrayList<Card>();
+        Random rand = new Random();
+        // random ukuran deck
+        int deckSize = rand.nextInt(61-40) + 40;
+        // random index daftar card untuk dimasukkan ke deck
+        while (result.size() < deckSize) {
+            int randIdx = rand.nextInt(listCard.size());
+            result.add(listCard.get(randIdx));
+        }
+        // memasukkan ke dalam deck
+        for (Card card : result) {
+            this.deck.push(card);
+        }
+        
     }
 
     public List<Card> draw() {
